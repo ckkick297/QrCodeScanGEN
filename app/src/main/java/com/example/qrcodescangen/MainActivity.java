32367a144d95028ca521ff8230b19e3b4a8f28c5
tukku <ckkick297@gmail.com>
2020-08-08 14:35:56 +0530

    init Comment

--- app/src/main/java/com/example/qrcodescangen/MainActivity.java
+++ app/src/main/java/com/example/qrcodescangen/MainActivity.java
@@ -3,13 +3,27 @@ package com.example.qrcodescangen;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.zxing.integration.android.IntentIntegrator;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        findViewById(R.id.btn_scan_qr).setOnClickListener(this);
+        findViewById(R.id.btn_generate_qr).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.btn_scan_qr){
+
+        }else if (v.getId() == R.id.btn_generate_qr){
+
+        }
     }
 }
