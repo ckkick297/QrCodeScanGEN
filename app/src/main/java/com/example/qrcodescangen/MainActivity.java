cdfefc2049cfea98465307fa34b54cc45911ba30
tukku <ckkick297@gmail.com>
2020-08-08 16:08:59 +0530

    init Comment

--- app/src/main/java/com/example/qrcodescangen/MainActivity.java
+++ app/src/main/java/com/example/qrcodescangen/MainActivity.java
@@ -1,13 +1,33 @@
 package com.example.qrcodescangen;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Point;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Display;
 import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.zxing.WriterException;
 import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+import androidmads.library.qrgenearator.QRGContents;
+import androidmads.library.qrgenearator.QRGEncoder;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    QRGEncoder qrgEncoder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,9 +41,84 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
         if (v.getId() == R.id.btn_scan_qr){
+            new IntentIntegrator(this).setCaptureActivity(ScannerActivity.class).initiateScan();
 
         }else if (v.getId() == R.id.btn_generate_qr){
 
+            String inputValue = "This is QR generate text";
+            Bitmap bitmap = null;
+
+            WindowManager manager = (WindowManager) getSystemService(WINDOW_SERVICE);
+            Display display = manager.getDefaultDisplay();
+            Point point = new Point();
+            display.getSize(point);
+            int width = point.x;
+            int height = point.y;
+            int smallerDimension = width < height ? width : height;
+            smallerDimension = smallerDimension * 3 / 4;
+
+            qrgEncoder = new QRGEncoder(inputValue, null, QRGContents.Type.TEXT, smallerDimension);
+            try {
+                bitmap = qrgEncoder.encodeAsBitmap();
+            } catch (WriterException e) {
+                Log.v("qr", e.toString());
+            }
+
+            AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+            LinearLayout linearLayout = new LinearLayout(this);
+            linearLayout.setOrientation(LinearLayout.VERTICAL);
+
+            TextView textViewValue = new TextView(this);
+            textViewValue.setText("Value :"+inputValue);
+
+            ImageView imageViewQR = new ImageView(this);
+            imageViewQR.setImageBitmap(bitmap);
+
+            linearLayout.addView(textViewValue);
+            linearLayout.addView(imageViewQR);
+
+            alertDialog.setView(linearLayout);
+            alertDialog.setTitle("QR generated");
+            alertDialog.show();
+
         }
     }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        //We will get scan results here
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        //check for null
+        if (result != null) {
+            if (result.getContents() == null) {
+                Toast.makeText(this, "Scan Cancelled", Toast.LENGTH_LONG).show();
+            } else {
+                //show dialogue with result
+                showResultDialogue(result.getContents());
+            }
+        } else {
+            // This is important, otherwise the result will not be passed to the fragment
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    //method to construct dialogue with scan results
+    public void showResultDialogue(final String result) {
+        AlertDialog.Builder builder;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            builder = new AlertDialog.Builder(this, android.R.style.Theme_Material_Dialog_Alert);
+        } else {
+            builder = new AlertDialog.Builder(this);
+        }
+        builder.setMessage("Found : " + result)
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // do nothing
+                        dialog.dismiss();
+                    }
+                })
+                .show();
+    }
+
 }
